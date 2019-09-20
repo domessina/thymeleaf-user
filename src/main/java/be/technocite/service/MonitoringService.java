5c4fd5107ed4ac48252d7abaa4bdbb852aaca447
domessina <osef@hotmail.com>
2019-09-20 17:01:19 +0200

    trying to fix login_success_metric WIP

--- src/main/java/be/technocite/service/MonitoringService.java
+++ src/main/java/be/technocite/service/MonitoringService.java
@@ -19,18 +19,20 @@ public class MonitoringService {
     private static final String LOGIN_SUCCESS = "login_success";
     private static final String LOGIN_FAILED = "login_failed";
     private static final String LOGIN_SUCCESS_RATIO = "login_success_ratio";
+    private static Double successRatio = 0.0;
 
     void trackLogin(String email, boolean success) {
         if (success) {
-            meterRegistry.counter(LOGIN_SUCCESS, "email", email).increment();
+            meterRegistry.counter(LOGIN_SUCCESS, "toto", "toto").increment(1);
         } else {
-            meterRegistry.counter(LOGIN_FAILED, "email", email).increment();
+            meterRegistry.counter(LOGIN_FAILED, "toto", "toto").increment(1);
         }
         try {
             Counter loginSuccess = meterRegistry.get(LOGIN_SUCCESS).counter();
             Counter loginFailed = meterRegistry.get(LOGIN_FAILED).counter();
             //FIXME Idk why, but the decimals are wrong eg: (2+3) / 3 = 2 !!
-            meterRegistry.gauge(LOGIN_SUCCESS_RATIO, (loginSuccess.count() + loginFailed.count()) / loginSuccess.count());
+            successRatio = loginSuccess.count() + loginFailed.count() / loginSuccess.count();
+//            meterRegistry.(LOGIN_SUCCESS_RATIO, successRatio);
         } catch (MeterNotFoundException e) {
             logger.debug("meter not found ");
         }
