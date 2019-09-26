da2c45bb488820f006c9f6f80a13e182871bdd6a
domessina <domessina@users.noreply.github.com>
2019-09-26 16:58:15 +0200

    Update MonitoringService.java
    added grafana notes in MonitoringService for to calulate login ratio

--- src/main/java/be/technocite/service/MonitoringService.java
+++ src/main/java/be/technocite/service/MonitoringService.java
@@ -18,10 +18,28 @@ public class MonitoringService {
 
     void trackLogin(String email, boolean success) {
         if (success) {
-            // dans grafana quand on veut faire des opérations ENTRE métriques il faut avoir le meme metric name
+            // dans grafana quand on veut faire des opérations ENTRE métriques il faut avoir le meme metric name et les memes label
+            // pcq grafana n'autorise les opérations qu'entre la meme métrique. Du coup il faut donner un nom différent ET (voir plus bas)
+            // faire ignoringState(true) pour lui dire t'inquiète pas si les tags sont différents. 
             meterRegistry.counter(LOGIN_SUCCESS_RATIO, "state", "success").increment(1);
         } else {
             meterRegistry.counter(LOGIN_SUCCESS_RATIO, "state", "failed").increment(1);
         }
     }
+    
+    /*6 – Dans notre dashboard grafana, on choisit de démarrer une nouvelle query et là il nous reste à écrire celle(s)-ci.
+Une seule métrique est disponible « metric_pet_total » mais en vrai il en existe 3 et celles-ci peuvent être différenciées par leurs labels
+
+metric_pet_total{state="total"}
+metric_pet_total{state="found"}
+metric_pet_total{state="lost"}
+
+Et le plus important, le ratio d’animaux perdus sur le total (en %)
+
+(metric_pet_total{state="lost"} / ignoring(state) metric_pet_total{state="total"})*100
+
+Idem pour les trouvailles
+
+(metric_pet_total{state="found"} / ignoring(state) metric_pet_total{state="total"})*100
+*/
 }
